a613a79cf9a239cf923f6029c9a0be1429caefe4
2series <rihadv021@gmail.com>
2023-03-30 20:27:32 +0200

    added second dataframe

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -2,4 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+
+df2 = pd.DataFrame({"C": [1, 2, 3], "D": [4, 5, 6]})
